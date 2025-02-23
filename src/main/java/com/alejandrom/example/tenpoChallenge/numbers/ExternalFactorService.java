a27d6e17058e9d9b1c09561f11d41d5415e9241c
alejandroMesias <alejandromesiasu@gmail>
2025-02-23 11:52:39 -0500

    Alejo: initial External factor service created

---
+++ src/main/java/com/alejandrom/example/tenpoChallenge/numbers/ExternalFactorService.java
@@ -0,0 +1,11 @@
+package com.alejandrom.example.tenpoChallenge.numbers;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExternalFactorService {
+
+    public double getFactor(){
+        return 1.2;
+    }
+}
--- src/main/java/com/alejandrom/example/tenpoChallenge/numbers/NumbersService.java
+++ src/main/java/com/alejandrom/example/tenpoChallenge/numbers/NumbersService.java
@@ -1,12 +1,19 @@
 package com.alejandrom.example.tenpoChallenge.numbers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NumbersService {
 
+    private final ExternalFactorService externalFactorService;
+
+    @Autowired
+    public NumbersService(ExternalFactorService externalFactorService) {
+        this.externalFactorService = externalFactorService;
+    }
+
     public double getOperation(Integer num1, Integer num2){
-        double factor = 1.2;
-        return (num1.doubleValue() + num2.doubleValue()) * factor;
+        return (num1.doubleValue() + num2.doubleValue()) * externalFactorService.getFactor();
     }
 }
